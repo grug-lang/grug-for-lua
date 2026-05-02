d7516366d43cbcc803762fa14c39cda0e38ca25d
Sander Bos <sander@streamhpc.com>
2026-05-02 20:12:09 +0200

    feat: add examples/invalid_on_fn_name

---
+++ examples/invalid_on_fn_name/example.lua
@@ -0,0 +1,15 @@
+package.path = package.path .. ";../../?.lua"
+
+local grug = require("grug")
+
+local state = grug.init({
+	grug_files = { "animals/labrador-Dog.grug" },
+})
+
+state:register("print_string", function(state, string)
+	print(string)
+end)
+
+local file = state.mods["animals"]["labrador-Dog.grug"]
+local e = file:create_entity()
+e:invalid_on_fn_name()
