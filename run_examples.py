2bf46e7bdf37fd1a73499ee8aeaf62abb55bed3c
Sander Bos <sander@streamhpc.com>
2026-05-03 01:03:20 +0200

    feat: add run_examples.py

---
+++ run_examples.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+
+def run_examples():
+    for entry in os.scandir("examples"):
+        lua_file = os.path.join(entry.path, "example.lua")
+        print(f"--- Running {lua_file} ---")
+
+        try:
+            subprocess.run(
+                ["luajit", "example.lua"],
+                cwd=entry.path,  # Run from within the example directory
+                timeout=1,  # 1 second limit
+                check=False,  # Don't raise exception on non-zero exit
+            )
+        except subprocess.TimeoutExpired:
+            print(f"{entry.name}: Timed out after 1s.")
+
+
+if __name__ == "__main__":
+    run_examples()
